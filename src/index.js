89254ecb978ef66cc82494f8af38f3fabe2a20fb
sydalshghty <168644290+sydalshghty@users.noreply.github.com>
2025-02-18 17:30:41 +0200

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 //import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename='United-Website-Hanger'>
+    <BrowserRouter>
       <Master/>
     </BrowserRouter>
 );
